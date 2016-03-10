76418b4327bf3fd486f7dfa1d8227c31bb961ec4
Christian Brunotte <cb@lathspell.de>
2016-03-10 23:23:52 +0100

    Seems to be standard.

--- java_test_springboot_rest/src/main/java/de/lathspell/test/springboot/Application.java
+++ java_test_springboot_rest/src/main/java/de/lathspell/test/springboot/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-public class Main {
+public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
