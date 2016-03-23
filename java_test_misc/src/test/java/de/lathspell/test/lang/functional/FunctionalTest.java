5e553d2bede0b23a7817c2ba8b5f9289ddb57d08
Christian Brunotte <cbrunotte@netcologne.de>
2016-03-23 14:30:33 +0100

    Java8 functional programming

---
+++ java_test_misc/src/test/java/de/lathspell/test/lang/functional/FunctionalTest.java
@@ -0,0 +1,62 @@
+package de.lathspell.test.lang.functional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class FunctionalTest {
+
+    @Test
+    public void testArrays() {
+        List<Integer> oldList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // List
+        List<Integer> newList = oldList.stream().filter(x -> (x % 2) == 0).collect(Collectors.toList());
+        assertEquals(Arrays.asList(2, 4, 6, 8, 10), newList);
+
+        // Object Array
+        Integer[] newIntegerArray = oldList.stream().filter(x -> (x % 2) == 0).toArray(Integer[]::new);
+        assertArrayEquals(new Integer[]{2, 4, 6, 8, 10}, newIntegerArray);
+
+        // Primitive Array
+        int[] newIntArray = oldList.stream().filter(x -> (x % 2) == 0).mapToInt(x -> x).toArray();
+        assertArrayEquals(new int[]{2, 4, 6, 8, 10}, newIntArray);
+    }
+
+    @Test
+    public void createStream() {
+        // object stream from literal list
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+        assertEquals(5, integerStream.count());
+
+        // object stream from object array
+        Integer[] integerArray = new Integer[]{1, 2, 3, 4, 5};
+
+        Stream<Integer> integerStream2a = Stream.of(integerArray);
+        assertEquals(5, integerStream2a.count());
+
+        Stream<Integer> integerStream2b = Arrays.stream(integerArray);
+        assertEquals(5, integerStream2b.count());
+
+        // primitive stream from primitive array
+        int[] intArray = new int[]{1, 2, 3, 4, 5};
+        IntStream intStream = Arrays.stream(intArray);
+        assertEquals(5, intStream.count());
+    }
+
+    @Test
+    public void testMap() {
+        List<Integer> oldList = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> newList = oldList.stream().map(x -> x * 2).collect(Collectors.toList());
+        assertEquals(Arrays.asList(2, 4, 6, 8, 10), newList);
+    }
+}
