282208e596e71f751bb298b467ed57d5e5dd1607
Christian Brunotte <cb@lathspell.de>
2016-10-23 19:09:02 +0200

    Example for Java EE App on plain Tomcat Servlet Container

---
+++ java_test_ee7_rest_jpa_tomcat/src/main/java/de/lathspell/java_test_ee7_rest_jpa/frontend/rest/ApplicationConfig.java
@@ -0,0 +1,40 @@
+package de.lathspell.java_test_ee7_rest_jpa.frontend.rest;
+
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(de.lathspell.java_test_ee7_rest_jpa.frontend.rest.ArticleFacade.class);
+        resources.add(org.eclipse.persistence.jpa.rs.exceptions.JPARSExceptionMapper.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.EntityResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.MetadataResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.PersistenceResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.PersistenceUnitResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.QueryResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.SingleResultQueryResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.unversioned.EntityResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.unversioned.PersistenceResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.unversioned.PersistenceUnitResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.unversioned.QueryResource.class);
+        resources.add(org.eclipse.persistence.jpa.rs.resources.unversioned.SingleResultQueryResource.class);
+        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+    }
+
+}
---
+++ java_test_ee7_rest_jpa_tomcat/src/main/java/de/lathspell/java_test_ee7_rest_jpa/frontend/rest/ArticleFacade.java
@@ -0,0 +1,76 @@
+package de.lathspell.java_test_ee7_rest_jpa.frontend.rest;
+
+import de.lathspell.java_test_ee7_rest_jpa.config.PropertyValue;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import de.lathspell.java_test_ee7_rest_jpa.model.Article;
+import lombok.extern.slf4j.Slf4j;
+
+@RequestScoped
+@Path("/rest/article")
+@Slf4j
+public class ArticleFacade {
+
+    @Inject
+    @PropertyValue("greeter.name")
+    private String name;
+
+    private EntityManager em;
+
+    public ArticleFacade() {
+        // em = Persistence.createEntityManagerFactory("myPU").createEntityManager();
+    }
+
+    @POST
+    @Consumes({"application/xml", "application/json"})
+    public long create(Article entity) {
+        //  log.info("#42# foo="+foo);
+        log.info("#42# prop=" + name);
+        log.info("create: " + entity);
+
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.flush();
+        em.refresh(entity);
+        em.getTransaction().commit();
+
+        log.info("created: " + entity);
+        return entity.getId();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") Long id, Article entity) {
+        log.info("edit: " + id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void remove(@PathParam("id") Long id) {
+        log.info("remove: " + id);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces({"application/xml", "application/json"})
+    public Article get(@PathParam("id") Long id) {
+        log.info("get: " + id);
+
+        Article a = em.createQuery("SELECT a FROM Article a WHERE a.id = :id", Article.class).setParameter("id", id).getSingleResult();
+        log.info("get found: " + a);
+
+        return a;
+    }
+
+}
