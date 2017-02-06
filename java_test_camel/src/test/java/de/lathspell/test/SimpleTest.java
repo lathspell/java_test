e34e7625acd717bdc2e995aa55ffde8eae0cf560
Christian Brunotte <cb@lathspell.de>
2017-02-07 00:02:53 +0100

    Tests

--- java_test_camel/src/test/java/de/lathspell/test/SimpleTest.java
+++ java_test_camel/src/test/java/de/lathspell/test/SimpleTest.java
@@ -1,4 +1,4 @@
-package de.lathspell.test.java_test_camel;
+package de.lathspell.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
@@ -6,17 +6,20 @@ import static org.apache.camel.LoggingLevel.INFO;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.junit.Test;
 
 import de.lathspell.test.java_test_camel.model.Person;
 import de.lathspell.test.java_test_camel.model.PersonConverter;
 
 @Slf4j
-public class Main {
+public class SimpleTest {
 
-    private static boolean running = true;
+    private static boolean stopped = false;
 
-    public static void main(String[] args) throws Exception {
+    private String body;
 
+    @Test
+    public void test1() throws Exception {
         RouteBuilder builder = new RouteBuilder() {
             @Override
             public void configure() {
@@ -38,7 +41,7 @@ public class Main {
                         .routeId("runOnce")
                         .to("direct:findFile");
                  */
-                from("file://src/main/resources/inputs/?include=input.*\\.txt&delay=1000&noop=true")
+                from("file://src/main/resources/inputs/?include=input.*\\.txt&noop=true")
                         .routeId("findFile")
                         .log(INFO, "### Found file: |${in.body}|")
                         .to("log:Received?level=WARN&showAll=true&multiline=true") /* of questionable use */
@@ -48,14 +51,15 @@ public class Main {
                         .log(INFO, "### Body is now: |${in.body}|")
                         .validate(body().regex("^Hello .*$"))
                         .log(INFO, "### Body is now: |${in.body}|")
+                        .toF("bean:body", "${body}")
                         .convertBodyTo(Person.class)
                         .log(INFO, "### Body is now: |${in.body}|")
                         .marshal().json(JsonLibrary.Jackson)
                         .log(INFO, "### Body is now: |${in.body}|")
                         .to("file://outputs/")
                         .log(INFO, "### Finished")
-                        .bean(Main.class, "stop");
-                // .to("controlbus:route?routeId=findFile&action=stop&async=true");
+                        .bean(SimpleTest.class, "stop");
+                // alternatively: .to("controlbus:route?routeId=findFile&action=stop&async=true");
             }
         };
 
@@ -69,16 +73,22 @@ public class Main {
         myCamelContext.addRoutes(builder);
         myCamelContext.start();
         log.info("------- started ------");
-        while (running) {
+        while (!stopped) {
             log.info("------- sleeping ------");
             Thread.sleep(1000 * 1);
         }
         log.info("------- stopping ------");
         myCamelContext.stop();
         log.info("------- end ------");
+        log.info("------- body: " + body + " ------");
     }
 
     public static void stop() {
-        running = false;
+        log.info("-------- stop requested ---------");
+        stopped = true;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 }
