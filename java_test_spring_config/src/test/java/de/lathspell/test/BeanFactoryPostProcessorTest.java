09a6ea39ef72db3eb7030dd3e8bd6e2f1d9b585b
Christian Brunotte <cb@lathspell.de>
2017-03-07 23:00:02 +0100

    Refactored

--- java_test_spring_config/src/test/java/de/lathspell/test/BeanFactoryPostProcessorTest.java
+++ java_test_spring_config/src/test/java/de/lathspell/test/BeanFactoryPostProcessorTest.java
@@ -2,12 +2,14 @@ package de.lathspell.test;
 
 import java.time.LocalDate;
 
+import lombok.extern.slf4j.Slf4j;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -18,10 +20,24 @@ import de.lathspell.test.model.Person;
  * Uses a BeanFactoryPostProcessor to modify an object at the time it is instantiated.
  */
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {BeanFactoryPostProcessorTestConfiguration.class, NewbornPersonBeanFactoryPostProcessor.class})
+@ContextConfiguration(classes = {BeanFactoryPostProcessorTest.class, NewbornPersonBeanFactoryPostProcessor.class})
+@Slf4j
 public class BeanFactoryPostProcessorTest {
 
-    /** Created by BeanFactoryPostProcessorTestConfiguration.newbornFactory and modifed by NewbornPersonBeanFactoryPostProcessor. */
+    /**
+     * Creating a Person object without setting the birthday.
+     */
+    @Bean("newborn")
+    public Person newbornFactory() {
+        log.info("Creating person");
+        Person p = new Person("Lilly", "Lu", null);
+        log.info("Created: " + p);
+        return p;
+    }
+
+    /**
+     * Created by BeanFactoryPostProcessorTestConfiguration.newbornFactory and modifed by NewbornPersonBeanFactoryPostProcessor.
+     */
     @Autowired
     private Person newborn;
 
