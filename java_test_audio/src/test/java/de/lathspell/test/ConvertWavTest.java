5855514156bec75e7a198eea2392cd399282bdfb
Christian Brunotte <cbrunotte@netcologne.de>
2018-11-19 15:22:25 +0100

    fixed

--- java_test_audio/src/test/java/de/lathspell/test/ConvertWavTest.java
+++ java_test_audio/src/test/java/de/lathspell/test/ConvertWavTest.java
@@ -1,14 +1,16 @@
 package de.lathspell.test;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
@@ -20,20 +22,24 @@ public class ConvertWavTest {
     public void convert() throws Exception {
 
         byte[] input = IOUtils.toByteArray(new FileInputStream("src/test/resources/input.wav"));
-
         ByteArrayInputStream inputBis = new ByteArrayInputStream(input);
 
-        AudioFormat frmt = new AudioFormat(44100, 8, 1, true, false);
+        ByteOutputStream bos = convert16to8bit(inputBis);
 
-        AudioInputStream ais = new AudioInputStream(inputBis, frmt, input.length);
-
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        AudioSystem.write(ais, AudioFileFormat.Type.WAVE, bos);
-
-        // check
-        byte[] output = bos.toByteArray();
+        byte[] output = bos.getBytes();
+        IOUtils.write(output, new FileOutputStream("src/test/resources/output.wav"));
         byte[] expected = IOUtils.toByteArray(new FileInputStream("src/test/resources/expected.wav"));
         assertArrayEquals(expected, output);
+    }
+
+    private ByteOutputStream convert16to8bit(InputStream is) throws Exception {
+        AudioFormat format = new AudioFormat(44100, 8, 1, true, true);
+        AudioInputStream in = AudioSystem.getAudioInputStream(is);
+        AudioInputStream convert = AudioSystem.getAudioInputStream(format, in);
+
+        ByteOutputStream bos = new ByteOutputStream();
+        AudioSystem.write(convert, AudioFileFormat.Type.WAVE, bos);
+        return bos;
     }
 
 }
