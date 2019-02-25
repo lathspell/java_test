7e2c0de74500b02c0ae4c74719b090126571e1d1
Christian Brunotte <cb@lathspell.de>
2019-02-25 23:10:02 +0100

    works now

--- java_test_springboot_graylog/src/main/java/de/lathspell/test/services/Thermometer.java
+++ java_test_springboot_graylog/src/main/java/de/lathspell/test/services/Thermometer.java
@@ -3,6 +3,7 @@ package de.lathspell.test.services;
 import java.util.Random;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,14 @@ public class Thermometer {
     private final Random rnd = new Random();
 
     @Scheduled(fixedRate = 1000L)
-    public void temperature() {
+    public void temperature1() {
+        MDC.put("window", "left");
+        log.info("Temperature " + rnd.nextInt(60));
+    }
+
+    @Scheduled(fixedRate = 1000L)
+    public void temperature2() {
+        MDC.put("window", "right"); // same thread as temperature1 - only used for demo purpose
         log.info("Temperature " + rnd.nextInt(60));
     }
 
